edd049b07212c2721e85c02249aeb3fa7f4b2bf7
hoaxdream <hoaxdream@gmail.com>
2022-02-12 07:58:48 +0800

    Reworked

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,9 +1,3 @@
-;;; early-init.el --- Early initialization -*- lexical-binding: t -*-
-
-;;; Commentary:
-
-;;; Code:
-
 ;; Initialise installed packages
 (setq package-enable-at-startup t)
 
@@ -20,24 +14,15 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq inhibit-splash-screen t)
-(setq use-dialog-box t)
 (setq use-file-dialog nil)
 
-(setq-default frame-title-format '("%b  -  GNU Emacs"))
-(setq inhibit-startup-screen t)
-(setq inhibit-startup-buffer-menu t)
-
-;; Line numbering
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
-
-;;(add-hook 'prog-mode-hook 'hl-line-mode)
+;; Prevent unwanted runtime builds in gccemacs
+(setq comp-deferred-compilation nil)
 
 ;; Disable backups
-(setq backup-inhibited t)
-(setq auto-save-list-file-prefix nil)
-(setq create-lockfiles nil)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
+(setq make-backup-files nil
+      auto-save-default nil
+      create-lockfiles nil)
 
 ;; Indention
 (setq-default indent-tabs-mode t)
@@ -49,23 +34,21 @@
 (setq-default org-display-custom-times t)
 (setq-default org-time-stamp-custom-formats '("<%a %b %d %Y>" . "<%a %b %d %Y %I:%M %p>"))
 
-;; Increase font size
-;;(set-face-attribute 'default nil :height 140)
-(set-face-attribute 'default nil :font "Inconsolata" :height 140)
+;; Font
+(set-face-attribute 'default nil :font "Inconsolata" :height 150)
 
-(require 'package)
+;; Comment color
+(set-face-foreground 'font-lock-comment-face "#7daea3")
 
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+;; Line numbering
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
-;;(unless package-archive-contents
-;;  (package-refresh-contents))
-
-;; if not yet installed, install package use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+;; themes
+;;(add-to-list 'custom-theme-load-path
+;;             (file-name-as-directory
+;;               (expand-file-name "~/.config/emacs/themes")))
+;;
+;;(load-theme 'gruvbox t)
 
 (require 'org)
 (org-babel-load-file "/home/hoaxdream/.config/emacs/init.org")
-
-;;; early-init.el ends here
