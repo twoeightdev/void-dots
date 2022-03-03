8ff2bba39f534c2ef1f8bbe463da0a87d7dafa70
hoaxdream <hoaxdream@gmail.com>
2022-03-03 21:18:04 +0800

    Some plugins

---
+++ .config/nvim/lua/plugins/due.lua
@@ -0,0 +1,23 @@
+local status_ok, due_nvim = pcall(require, "due_nvim")
+if not status_ok then
+	return
+end
+
+due_nvim.setup{
+    draw = 0,
+    clean = 0,
+    redraw = 0,
+    prescript = "due: ",
+    prescript_hi = "Comment",
+    due_hi = "String",
+    ft = "*.md",
+    today = "TODAY",
+    today_hi = "Character",
+    overdue = "OVERDUE",
+    overdue_hi = "Error",
+    date_hi = "Conceal",
+    pattern_start = "<",
+    pattern_end = ">",
+    use_clock_time = false,
+    default_due_time = "noon",
+}
---
+++ .config/nvim/lua/plugins/todo.lua
@@ -0,0 +1,20 @@
+local status_ok, todo_comments = pcall(require, "todo-comments.config")
+if not status_ok then
+	return
+end
+
+todo_comments.setup {
+    signs = false,
+    keywords = {
+        FIX = {
+            icon = " ",
+            color = "error",
+            alt = { "FIXME", "BUG", "FIXIT", "FIX", "ISSUE" }
+        },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning", alt = { "FUCK", "SHIT", "BAD" } },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+    },
+}
