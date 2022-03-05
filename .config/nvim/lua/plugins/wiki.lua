edd3cae0dfedaede8276048a3eda8bd222b5bf60
hoaxdream <hoaxdream@gmail.com>
2022-03-05 19:48:37 +0800

    Removed vimwiki and use wiki.vim

---
+++ .config/nvim/lua/plugins/wiki.lua
@@ -0,0 +1,47 @@
+-- Wiki.vim - Minimal wiki
+vim.g.wiki_global_load = 0
+vim.g.wiki_root = "~/.config/dev/personal/wiki"
+vim.g.wiki_filetypes = { "md" }
+vim.g.wiki_link_extension = ".md"
+
+vim.g.wiki_link_target_type = 'md'
+vim.g.wiki_write_on_nav = 1
+vim.g.wiki_list_todo = 1
+vim.g.wiki_mappings_use_defaults = 1    -- disable all mappings
+
+-- List.vim - Toggle checkbox for wiki.vim
+vim.g.lists_filetypes = { "md", "markdown" }
+
+
+-- Export
+vim.g.wiki_export = {
+    args = "--pdf-engine=tectonic",
+    from_format = "markdown",
+    ext = "pdf",
+    output = "pdf/",
+}
+
+vim.g.wiki_journal = {
+    index_use_journal_scheme = 1,
+    name = "diary",
+    frequency = "daily",
+    date_format = {
+        daily = "%Y-%m-%d",
+        weekly = "%Y_w%V",
+        monthly = "%Y_m%m",
+    }
+}
+
+--local cmd = vim.cmd
+
+
+
+-- Calendar
+-- vim.g.calendar_diary = "~/.config/dev/personal/wiki/diary"
+--
+-- vim.g.calendar_filetype = "markdown"
+-- vim.g.calendar_diary_path_pattern = "{YYYY}-{MM}-{DD}{EXT}"
+-- vim.g.calendar_diary_extensions = ".md"
+
+
+--vim.g.bullets_enabled_file_types = { "markdown" }
