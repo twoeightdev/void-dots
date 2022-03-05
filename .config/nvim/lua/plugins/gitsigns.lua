9583a3a35b1c5b0d5cc6391385ab048934f548e5
hoaxdream <hoaxdream@gmail.com>
2022-03-05 19:44:39 +0800

    Tidy up

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,11 +5,36 @@ end
 
 gitsigns.setup {
     signs = {
-        add = { hl = "GitSignsAdd", text = "++", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "+", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "=", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        add = {
+            hl = "GitSignsAdd",
+            text = "++",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn"
+        },
+        change = {
+            hl = "GitSignsChange",
+            text = "+",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        },
+        delete = {
+            hl = "GitSignsDelete",
+            text = "_",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        topdelete = {
+            hl = "GitSignsDelete",
+            text = "=",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        changedelete = {
+            hl = "GitSignsChange",
+            text = "~",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        },
     },
     signcolumn = true, -- toggle with :Gitsigns toggle_signs
     numhl = false, -- toggle with :Gitsigns toggle_numhl
@@ -20,7 +45,7 @@ gitsigns.setup {
         follow_files = true,
     },
     attach_to_untracked = true,
-    current_line_blame = false, -- toggle with :Gitsigns toggle_current_line_blame
+    current_line_blame = false, -- :Gitsigns toggle_current_line_blame
     current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- eol | overlay | right_align
