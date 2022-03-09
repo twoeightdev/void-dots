a35193da6692b975e3596adafafb86f021f96136
hoaxdream <hoaxdream@gmail.com>
2022-03-09 23:03:43 +0800

    trailing blankline indent

--- .config/nvim/lua/plugins/blankline.lua
+++ .config/nvim/lua/plugins/blankline.lua
@@ -5,7 +5,9 @@ end
 
 indent_blankline.setup {
     char = "▏",
+    show_trailing_blankline_indent = true,
     show_first_indent_level = true,
+    --show_current_context = true,
     filetype_exclude = {
         'help',
         'git',
