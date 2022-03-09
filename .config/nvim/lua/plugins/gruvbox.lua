fa7ef7fed090b97ea4cdcd86cc89bf630eb0ca1a
hoaxdream <hoaxdream@gmail.com>
2022-03-09 23:05:35 +0800

    transparent background

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -7,6 +7,7 @@ vim.o.background = "dark" -- dark or light
 vim.g.gruvbox_flat_style = "hard" -- dark or hard
 vim.g.gruvbox_italic_comments = false
 vim.g.showmode = false
+vim.g.gruvbox_transparent = true    -- looks good with st opacity
 vim.g.gruvbox_dark_sidebar = false
 vim.g.gruvbox_theme = {     -- Fix colors in neorg
     TSNote = {
@@ -16,6 +17,12 @@ vim.g.gruvbox_theme = {     -- Fix colors in neorg
     TSDanger = {
         bg = "none",
         fg = "none"
+    },
+    WhichKeyFloat = {
+        bg = "none",
+    },
+    Pmenu = {
+        bg = "none",
     }
 }
 
